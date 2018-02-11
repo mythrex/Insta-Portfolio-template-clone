154eac44cfb69f66842f8387a2877770f8d002a5
Mythrex <shivam.kaushik73@gmail.com>
2018-02-12 00:34:22 +0530

    Add materialize sass to project

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const jquery = require('jquery');
-const materializeCss = require('./css/materialize.min.css');
+const materializeCss = require('./sass/materialize.scss');
 const materializeJS = require('./js/materialize.min.js');
 const commonStyle = require('./sass/app.sass');
 const script = require('./js/script.js')
